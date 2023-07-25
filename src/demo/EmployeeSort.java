c4827088e337d823d7c7a9f89fdd7ab26954cce8
Poornasamy S <poornasamys@gmail.com>
2023-07-25 20:41:25 +0530

    Update EmployeeSort.java

--- src/demo/EmployeeSort.java
+++ src/demo/EmployeeSort.java
@@ -1,8 +1,10 @@
 package demo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 class Employee {
@@ -44,32 +46,91 @@ class Employee {
 public class EmployeeSort {
 
 	public static void main(String[] args) {
-		List<Employee> employees = Arrays.asList(
-		new Employee(101, "Dinesh", 2000L),
-		new Employee(301, "Bala", 3000L),
-		new Employee(201, "Chandru", 2500L),
-		new Employee(401, "Anand", 5000L));
+		List<Employee> employees = Arrays.asList(new Employee(101, "Dinesh", 2000L), new Employee(301, "Bala", 3000L),
+				new Employee(201, "Chandru", 2500L), new Employee(401, "Anand", 5000L));
 
 		System.out.println("Before Sorting");
 		System.out.println("--------------\n");
-		employees.forEach(employee -> System.out.println(employee.getId() + " " + employee.getName() + " " + employee.getSalary()));
+		employees.forEach(employee -> System.out
+				.println(employee.getId() + " " + employee.getName() + " " + employee.getSalary()));
 
-		List<Employee> sortedEmployeesByName = employees.stream().sorted(Comparator.comparing(Employee::getName).reversed())
-				.collect(Collectors.toList());
-		System.out.println("\nAfter Sorting by employee name");
-		System.out.println("--------------------------------\n");
-		sortedEmployeesByName.forEach(employee -> System.out.println(employee.getId() + " " + employee.getName() + " " + employee.getSalary()));
-		
-		List<Employee> sortedEmployeesById = employees.stream().sorted(Comparator.comparing(Employee::getId).reversed())
-				.collect(Collectors.toList());
-		System.out.println("\nAfter Sorting  by employee id");
-		System.out.println("-------------------------------\n");
-		sortedEmployeesById.forEach(employee -> System.out.println(employee.getId() + " " + employee.getName() + " " + employee.getSalary()));
-		
-		List<Employee> sortedEmployeesBySalary = employees.stream().sorted(Comparator.comparing(Employee::getSalary).reversed())
-				.collect(Collectors.toList());
-		System.out.println("\nAfter Sorting  by employee salary");
-		System.out.println("-----------------------------------\n");
-		sortedEmployeesBySalary.forEach(employee -> System.out.println(employee.getId() + " " + employee.getName() + " " + employee.getSalary()));
+		System.out.println("\n1. Sort by id in ascending");
+		System.out.println("2. Sort by name in ascending");
+		System.out.println("3. Sort by salary in ascending");
+		System.out.println("4. Sort by id in descending");
+		System.out.println("5. Sort by name in descending");
+		System.out.println("6. Sort by salary in descending");
+		System.out.println("0. Exit");
+		System.out.println("Sort by id in ascending by default");
+
+		try (Scanner sc = new Scanner(System.in)) {
+			int option = 0;
+			do {
+				System.out.println("\nEnter your choice");
+				option = sc.nextInt();
+				List<Employee> sortedEmployees = new ArrayList<>();
+				switch (option) {
+				case 0:
+					System.out.println("Exiting...");
+					break;
+				case 1:
+					System.out.println("\nAfter Sort by id in ascending");
+					System.out.println("-------------------------------\n");
+					sortedEmployees = employees.stream().sorted(Comparator.comparing(Employee::getId))
+							.collect(Collectors.toList());
+					sortedEmployees.forEach(employee -> System.out
+							.println(employee.getId() + " " + employee.getName() + " " + employee.getSalary()));
+					break;
+				case 2:
+					System.out.println("\nAfter Sort by name in ascending");
+					System.out.println("---------------------------------\n");
+					sortedEmployees = employees.stream().sorted(Comparator.comparing(Employee::getName))
+							.collect(Collectors.toList());
+					sortedEmployees.forEach(employee -> System.out
+							.println(employee.getId() + " " + employee.getName() + " " + employee.getSalary()));
+					break;
+				case 3:
+					System.out.println("\nAfter Sort by salary in ascending");
+					System.out.println("-----------------------------------\n");
+					sortedEmployees = employees.stream().sorted(Comparator.comparing(Employee::getSalary))
+							.collect(Collectors.toList());
+					sortedEmployees.forEach(employee -> System.out
+							.println(employee.getId() + " " + employee.getName() + " " + employee.getSalary()));
+					break;
+				case 4:
+					System.out.println("\nAfter Sort by id in descending");
+					System.out.println("--------------------------------\n");
+					sortedEmployees = employees.stream().sorted(Comparator.comparing(Employee::getId).reversed())
+							.collect(Collectors.toList());
+					sortedEmployees.forEach(employee -> System.out
+							.println(employee.getId() + " " + employee.getName() + " " + employee.getSalary()));
+					break;
+				case 5:
+					System.out.println("\nAfter Sort by name in descending");
+					System.out.println("----------------------------------\n");
+					sortedEmployees = employees.stream().sorted(Comparator.comparing(Employee::getName).reversed())
+							.collect(Collectors.toList());
+					sortedEmployees.forEach(employee -> System.out
+							.println(employee.getId() + " " + employee.getName() + " " + employee.getSalary()));
+					break;
+				case 6:
+					System.out.println("\nAfter Sort by salary in descending");
+					System.out.println("------------------------------------\n");
+					sortedEmployees = employees.stream().sorted(Comparator.comparing(Employee::getSalary).reversed())
+							.collect(Collectors.toList());
+					sortedEmployees.forEach(employee -> System.out
+							.println(employee.getId() + " " + employee.getName() + " " + employee.getSalary()));
+					break;
+				default:
+					System.out.println("\nAfter Sort by id in ascending by default");
+					System.out.println("------------------------------------------\n");
+					sortedEmployees = employees.stream().sorted(Comparator.comparing(Employee::getId))
+							.collect(Collectors.toList());
+					sortedEmployees.forEach(employee -> System.out
+							.println(employee.getId() + " " + employee.getName() + " " + employee.getSalary()));
+				}
+			} while (option != 0);
+
+		}
 	}
 }
